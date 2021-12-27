c78b4152e97cf85370fdf37f10e885f08280cfe5
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2021-12-27 10:17:33 +0900

    exit지점에 도달하면 게임 종료

---
+++ player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rigid;
+    public float maxSpeed;
+    float h;
+    float v;
+    bool isHorizonMove;
+    Vector3 pos;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+
+        //자동차 가로세로 움직임, y position freeze하기 
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp= Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        if (hDown)
+            isHorizonMove = true;
+        else if (vDown)
+            isHorizonMove = false;
+
+    }
+    void FixedUpdate()
+    {
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v); //세로로는 움직x
+        rigid.velocity = moveVec * maxSpeed;
+        pos = rigid.position;
+
+        // exit 선이 x=1 선에 있다고 가정
+        if (pos.x>1)  
+        {
+            Time.timeScale = 0; //시간 멈춤
+            Debug.Log("탈출 성공");
+        }
+        
+      
+    }
+
+    //부딪히면 멈추는 방식
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+      //  if (collision.GameObject.tag == "Finish")
+       // {
+        //    Time.timeScale = 0; //멈춤
+       //     Debug.Log("탈출 성공");
+
+      //  }
+    }
+
+
+}
